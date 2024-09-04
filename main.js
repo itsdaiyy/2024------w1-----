8af651025d51fd60ad3e48c274a443a43784cee0
Ollie D <behoya11@gmail.com>
2024-09-04 12:12:24 +0800

    feat: 新增動態特效

---
+++ main.js
@@ -0,0 +1,36 @@
+// use a script tag or an external JS file
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // gsap code here!
+  gsap.to(".banner-img", {
+    "--width": "0",
+    duration: 0.8,
+    delay: 0.3,
+    ease: "power3.inOut",
+  });
+
+  gsap.to(".animation-to-right", {
+    scrollTrigger: {
+      trigger: ".intro",
+      start: "top 80%",
+      end: "top 60% ",
+      scrub: 1,
+    },
+    "--width": "0",
+  });
+
+  document.querySelectorAll(".animation-fadeup").forEach((el) => {
+    gsap.from(el, {
+      scrollTrigger: {
+        trigger: el,
+        start: "top 80%",
+        end: () => `+=${el.offsetHeight}px`,
+        scrub: 1,
+        markers: true,
+      },
+      opacity: 0,
+      y: 20,
+    });
+  });
+});
